08f36601cc3e496031df9788133c833ef5e6119a
Nikola Vlahovic <nikola.vlahovic2401@gmail.com>
2018-05-16 22:10:36 +0200

    Add basic standard and prey-carry-capped Lotka-Voltera models
    With graphs
    
    Signed-off-by: Nikola Vlahovic <nikola.vlahovic2401@gmail.com>

---
+++ plot_figures.m
@@ -0,0 +1,7 @@
+function [] = plot_figures(ts, ys, equations)
+    plot(ts, ys)
+    legend('plen', 'predator')
+    figure();
+    plot(ys(:,1),ys(:,2)); hold on               % plot solution in phase plane
+    xlabel('plen'); ylabel('predator')
+    vectfield(equations,0:.8:7.5,0:.8:10.5); hold off    % plot vector field
