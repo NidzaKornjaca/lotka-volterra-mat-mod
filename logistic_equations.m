af5a728a3a61d5dbc09c9629e052817e739a4405
Nikola Vlahovic <nikola.vlahovic2401@gmail.com>
2018-05-27 15:21:40 +0200

    Reformat some matlab files
    Signed-off-by: Nikola Vlahovic <nikola.vlahovic2401@gmail.com>

--- logistic_equations.m
+++ logistic_equations.m
@@ -1,10 +1,14 @@
 params;
 
 lv_logistic = @ (t, state) [
-    a * state(1) * ( 1 - (state(1))/u) - b * state(1) * state(2);
-    c * state(1) * state(2) - m * state(2)
+    a * state(1) * ( 1 - (state(1))/u) -
+    b * state(1) * state(2);
+    c * state(1) * state(2) -
+    m * state(2)
 ];
 
-[ts, ys] = ode45(lv_logistic, [0, 15], [p_init;g_init]);
+[ts, ys] = ode45(
+    lv_logistic, [0, 15], [p_init;g_init]
+);
 
 plot_figures(ts, ys, lv_logistic);
--- standard_equations.m
+++ standard_equations.m
@@ -3,8 +3,10 @@ params;
 lv_standard = @(t, state) [
     a * state(1) - b * state(1) * state(2);
     c * state(1) * state(2) - m * state(2)
-    ];
+];
 
-[ts, ys] = ode45(lv_standard, [0, 10], [p_init;g_init]);
+[ts, ys] = ode45(
+    lv_standard, [0, 10], [p_init;g_init]
+);
 
 plot_figures(ts, ys, lv_standard);
